34d70952c7198ddc1ac9983b5407c278a0d654d6
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-04-29 21:56:20 +0300

    Created structures that define a graph using adjacence list

--- Graph.h
+++ Graph.h
@@ -5,5 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct AdjListNode{
+    char* actorName;
+    struct AdjListNode* next;
+}AdjListNode;
+
+typedef struct AdjList{
+    struct AdjListNode* head;
+}AdjList;
+
+typedef struct Graph{
+    int nodesCount;
+    struct AdjList* nodes;
+}Graph;
+
 
 #endif
