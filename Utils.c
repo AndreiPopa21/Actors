683e7ec51fd4b1076ed3a015ca6cd72937fcc6ad
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-04-30 13:58:21 +0300

    Implemented function for checking duplicate before adding a new actor list

--- Utils.c
+++ Utils.c
@@ -49,10 +49,27 @@ int checkNameDuplicate(Graph** graph, char* name){
     if(!(*graph)){
         return 1;
     }
-    if(name[0]=='\0'){
+
+    if(!((*graph)->lists)){
         return 1;
     }
 
+    if(name[0]=='\0'){
+        return 1;
+    }
+    int currentSize = (*graph)->currentSize;
+    int hasDuplicate = 0;
+    int i;
+
+    for(i = 0; i < currentSize; i++){
+        char* actorName = (*graph)->lists[i]->head->actorName;
+        if(!strcmp(name,actorName)){
+            hasDuplicate = 1;
+        }
+    }
+    if(hasDuplicate){
+        return 1;
+    }
     return 0;
 }
 
